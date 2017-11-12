a1f30a107e94e456e6c7e95e333e273ec9b10674
Cbkhare <ChitrabasuKhare89@gmail.com>
2017-11-12 22:49:46 +0530

    Adding Abstract Tree class

---
+++ adt_tree.py
@@ -0,0 +1,58 @@
+class Tree(object):
+    """Abstract class for Tree"""
+
+    class Position:
+        """Abstraction to location the position of single element"""
+
+        def element(self):
+            """return the element stored at this position"""
+            raise NotImplementedError('Must be implemented by subclass')
+
+        def __eq__(self, other):
+            """returns True if position represents the same location"""
+            raise NotImplementedError('Must be implemented by subclass')
+
+        def __ne__(self, other):
+            """return True id position doesn not represent the same location"""
+            return not (self == other)
+
+    # ------------ Abstract methods to be Implemented by concrete subclasses --
+    def root(self):
+        """Return the position representing the root else None if empty"""
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def parent(self, node):
+        """Return the position node representing the node's parent Else None"""
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def num_children(self, node):
+        """Return the number of child that position node have"""
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def children(self, node):
+        """Generate an iteration of Positions representing node's children"""
+        raise NotImplementedError('Must be implemented by subclass')
+
+    def __len__(self):
+        """Return the total number of nodes in the tree"""
+        raise NotImplementedError('Must be implemented by subclass')
+
+    # ------------ concrete methods to be Implemented in this class -----------
+    def is_root(self, node):
+        """Return True if Position node represents the root of the
+        tree"""
+        return self.root() == node
+
+    def is_leaf(self, node):
+        """Return True if Position node does not have any children"""
+        return self.num_children(node) == 0
+
+    def is_empty(self):
+        """Return True if tree is empty"""
+        return self.__len__() == 0
+
+
+
+
+
+    
